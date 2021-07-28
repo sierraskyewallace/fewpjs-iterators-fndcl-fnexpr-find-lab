94fc36bb4987458fd19dd30686eb5ec6ecaafd0e
sierraskyewallace <sskwallace@gmail.com>
2021-07-28 14:32:38 -0700

    commit tree faecea6e024705f7872727b38383a244c91a3607

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+
+// write a superBowlWin function that recieved an array of javscript objects as an argument
+// each object has 2 properties, year and result
+// use find() to test if result of each object is "W"
+// if it is, return the year of the win
+// if it is not, return "No winners"
+// if there are no winners, return "No winners"
+
+function superbowlWin(arr) {
+    var winner = arr.find(function(obj) {
+        return obj.result === "W";
+    });
+    if (winner) {
+        return winner.year;
+    } else {
+        return undefined;
+    }
+}
